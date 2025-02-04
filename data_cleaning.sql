8539be1e2a711ecf0082c3298f7a3cb99ebefa37
Aaryen D'souza <dc.aaryen@gmail.com>
2025-02-04 15:33:44 -0500

    Added Data and imported layoffs database

---
+++ data_cleaning.sql
@@ -0,0 +1,16 @@
+use world_layoffs;
+
+select * from layoffs;
+
+
+-- Remove Duplicates
+-- Standardize the Data
+-- Null Values or blank values
+-- Remove any columns
+
+
+create table layoffs_staging
+like layoffs;
+
+select * from layoffs_staging;
+
