9ef2abb524ea42dfb2f4ab2c9754a99a6f69847f
Aaryen D'souza <dc.aaryen@gmail.com>
2025-02-07 01:43:56 -0500

    Add EDA SQL script and improve formatting in data_cleaning.sql

---
+++ eda.sql
@@ -0,0 +1,43 @@
+use world_layoffs;
+
+select *
+from layoffs_staging2;
+
+select max(total_laid_off), max(percentage_laid_off)
+from layoffs_staging2;
+
+select *
+from layoffs_staging2
+where percentage_laid_off = 1
+order by total_laid_off desc;
+
+select *
+from layoffs_staging2
+where total_laid_off = 12000;
+select *
+from layoffs_staging2
+where percentage_laid_off = 1
+order by funds_raised_millions desc;
+
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+select min(date), max(date)
+from layoffs_staging2;
+
+select industry, sum(total_laid_off)
+from layoffs_staging2
+group by industry
+order by 2 desc;
+
+select country, sum(total_laid_off)
+from layoffs_staging2
+group by country
+order by 2 desc;
+
+select year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by year(`date`)
+order by 2 desc;
