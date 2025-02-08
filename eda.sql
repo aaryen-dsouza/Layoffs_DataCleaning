1c9215bc8d686f74a85b0c9fd0a28969dcb48e53
Aaryen D'souza <dc.aaryen@gmail.com>
2025-02-08 01:12:20 -0500

    Add additional SQL queries for monthly and company-wise layoffs analysis

--- eda.sql
+++ eda.sql
@@ -41,3 +41,32 @@ select year(`date`), sum(total_laid_off)
 from layoffs_staging2
 group by year(`date`)
 order by 2 desc;
+
+select substring(`date`, 1, 7) as month, sum(total_laid_off)
+from layoffs_staging2
+where substring(`date`, 1, 7) is not null
+group by 1
+order by 2 desc;
+
+SELECT company, YEAR(`date`) , SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY company;
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 desc;
+
+with Company_Year (company, years, total_laid_off) as
+         (SELECT company, YEAR(`date`), SUM(total_laid_off)
+          FROM layoffs_staging2
+          GROUP BY company, YEAR(`date`)),
+     company_year_rank as
+         (SELECT *,
+                 DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+          FROM Company_Year
+          WHERE years IS NOT NULL)
+select *
+from company_year_rank
+where Ranking <= 5;
